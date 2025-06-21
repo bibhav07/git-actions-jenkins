134e33ed7364da4f5e315ff4e0f63f9ce0137bd9
bibhav yadav <59442413+bibhav07@users.noreply.github.com>
2025-06-21 21:16:15 +0530

    Update main.js

--- main.js
+++ main.js
@@ -6,6 +6,9 @@ app.use("/hi", (req, res) => {
     res.status(200).json({ message: "hey, welcome...." })
 });
 
+app.use("/bye", (req, res) => {
+    res.status(200).json({ message: "bye...." })
+});
 app.listen( 3000, () => {
     console.log("app running on port 3000");
     
