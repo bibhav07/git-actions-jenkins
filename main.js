7083ff15eb7ce47e44a91cb34ee5a2c2f1183ae9
bibhav yadav <59442413+bibhav07@users.noreply.github.com>
2025-06-21 21:56:53 +0530

    Update main.js

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 
-app.use("/hi", (req, res) => {
+app.use("/hello", (req, res) => {
     res.status(200).json({ message: "hey, welcome...." })
 });
 
